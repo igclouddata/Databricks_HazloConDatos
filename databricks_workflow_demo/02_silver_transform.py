ae7f92520a264b54aa2f6de37f645c682aea9c62
igomez-2025 <igomezfg@gmail.com>
2025-11-20 16:29:45 -0500

    Update 02_silver_transform.py

--- databricks_workflow_demo/02_silver_transform.py
+++ databricks_workflow_demo/02_silver_transform.py
@@ -1,6 +1,21 @@
 # 02_silver_transform.py
 
-%run ./00_config
+# %run ./00_config
+
+# Ajusta esto al catálogo que estés usando
+CATALOG = "project_databricks_hcd"  # cámbialo si es otro
+
+BRONZE_SCHEMA = "demo_bronze"
+SILVER_SCHEMA = "demo_silver"
+GOLD_SCHEMA   = "demo_gold"
+
+spark.sql(f"USE CATALOG {CATALOG}")
+spark.sql(f"CREATE SCHEMA IF NOT EXISTS {BRONZE_SCHEMA}")
+spark.sql(f"CREATE SCHEMA IF NOT EXISTS {SILVER_SCHEMA}")
+spark.sql(f"CREATE SCHEMA IF NOT EXISTS {GOLD_SCHEMA}")
+
+print(f"Usando catálogo: {CATALOG}")
+print(f"Esquemas: {BRONZE_SCHEMA}, {SILVER_SCHEMA}, {GOLD_SCHEMA}")
 
 from pyspark.sql.functions import trim, lower, col
 
